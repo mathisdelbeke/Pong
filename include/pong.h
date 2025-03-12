ecdb815758172dc55fb1fbc65c667407d2c9c739
mathis <mathisdelbeke.2003@hotmail.com>
2025-03-12 12:41:16 +0100

    Enemy plays against user

--- include/pong.h
+++ include/pong.h
@@ -26,4 +26,21 @@ typedef struct {
     int radius;
 } Ball;
 
+static void make_players();
+static void init_player_struct(Player *p, int num, int x, int y, int width, int height);
+static void make_ball();
+static void first_render();
+static void init_window();
+static void init_drawing_area();
+static void init_key_presses();
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+static gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+static gboolean update_game(gpointer user_data);
+static void update_ball_pos();
+static void check_wall_collision();
+static void check_goal();
+static void check_player_collision(Player *player);
+static void update_enemy();
+static int predict_ball_y();
+
 #endif
--- src/pong.c
+++ src/pong.c
@@ -4,20 +4,21 @@
 #include <math.h>
 #include "pong.h"
 
-static int FIELD_WIDTH = 800;
-static int FIELD_HEIGHT = 500;
+static const int FIELD_WIDTH = 800;
+static const int FIELD_HEIGHT = 500;
+static const float MAX_BOUNCE_ANGLE = (75 * (G_PI / 180));
+static const int GOAL_WIDTH = 10;
 
-static int WIDTH_USER = 10;
-static int WIDTH_ENEMY = 10;
-static int HEIGHT_USER = 60;
-static int HEIGHT_ENEMY = 60;
+static int width_user = 10;
+static int width_enemy = 10;
+static int height_user = 60;
+static int height_enemy = 60;
 
-static int BALL_RAD = 10;
-static int MOVE_SPEED = 20;
-static float MAX_BOUNCE_ANGLE = (75 * (G_PI / 180));
-static int GOAL_WIDTH = 10;
+static int ball_radius = 10;
+static int user_speed = 20;
+static int enemy_speed = 20;
 
-static bool running = true;
+static bool enemy_catch_up = true;
 
 static Player user;
 static Player enemy;
@@ -26,40 +27,66 @@ static Ball ball;
 static GtkWidget *window = NULL;
 static GtkWidget *drawing_area = NULL;
 
-static void update_ball_pos() {
-    ball.pos.x += ball.dx;
-    ball.pos.y += ball.dy;
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    make_players();
+    make_ball();
+    first_render();
+    return 0;
 }
 
-static void check_wall_collision() {
-    if (ball.pos.y - ball.radius <= 0 || ball.pos.y + ball.radius >= FIELD_HEIGHT) {
-        ball.dy = -ball.dy;
-    }
+static void make_players() {
+    int x_user = 0 + GOAL_WIDTH;
+    int x_enemy = FIELD_WIDTH - width_enemy - GOAL_WIDTH;
+    int y_user = (FIELD_HEIGHT / 2) - (height_user / 2);
+    int y_enemy = (FIELD_HEIGHT / 2) - (height_enemy / 2);
+
+    init_player_struct(&user, 0, x_user, y_user, width_user, height_user);
+    init_player_struct(&enemy, 1, x_enemy, y_enemy, width_enemy, height_enemy);
 }
 
-static void check_player_collision(Player *player) {
-    if (ball.pos.x - ball.radius <= player->pos.x + player->size.width && ball.pos.x + ball.radius >= player->pos.x) {
-        if (ball.pos.y >= player->pos.y && ball.pos.y <= player->pos.y + player->size.height) {
-            float hit_position = (ball.pos.y - player->pos.y) / (float)player->size.height;
-            float bounce_angle = (hit_position - 0.5) * MAX_BOUNCE_ANGLE;
-
-            ball.dx = ball.speed * cos(bounce_angle) * (ball.dx < 0 ? 1 : -1);  
-            ball.dy = ball.speed * sin(bounce_angle);
-        }
-    }
+static void init_player_struct(Player *p, int num, int x, int y, int width, int height) {  
+    p->number = num;
+    p->pos.x = x;
+    p->pos.y = y;
+    p->size.width = width;
+    p->size.height = height;
 }
 
-static gboolean update_game(gpointer user_data) {
-    update_ball_pos();
-    check_wall_collision();
-    check_player_collision(&user);
-    check_player_collision(&enemy);
+static void make_ball() {
+    ball.pos.x = (FIELD_WIDTH / 2) - (ball_radius / 2);
+    ball.pos.y = (FIELD_HEIGHT / 2) - (ball_radius / 2);
+    ball.radius = ball_radius;
+    ball.dx = -10;      
+    ball.dy = 0;
+    ball.speed = 10;     
+}
 
-    if (ball.pos.x < 0) ball.pos.x = FIELD_WIDTH / 2;
-    if (ball.pos.x > FIELD_WIDTH) ball.pos.x = FIELD_WIDTH / 2;
-    
-    gtk_widget_queue_draw(drawing_area);
-    return TRUE;
+static void first_render() {
+    init_window();
+    init_drawing_area();
+    init_key_presses();
+    gtk_widget_show_all(GTK_WIDGET(window));
+    g_timeout_add(16, update_game, NULL);
+    gtk_main();
+}
+
+static void init_window() {
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Pong");
+    gtk_window_set_default_size(GTK_WINDOW(window), FIELD_WIDTH, FIELD_HEIGHT);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+}
+
+static void init_drawing_area() {
+    drawing_area = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
+    g_signal_connect(drawing_area, "draw", G_CALLBACK(on_draw_event), &user);
+}
+
+static void init_key_presses() {
+    g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press_event), NULL);
+    gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
 }
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
@@ -83,75 +110,89 @@ static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data
 static gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
     if (event->keyval == GDK_KEY_Up) {
         if (user.pos.y > 0) {
-            user.pos.y -= MOVE_SPEED;  
+            user.pos.y -= user_speed;  
         }
     } else if (event->keyval == GDK_KEY_Down) {
-        if (user.pos.y <= (FIELD_HEIGHT - user.size.height - MOVE_SPEED)) {
-            user.pos.y += MOVE_SPEED;
+        if (user.pos.y <= (FIELD_HEIGHT - user.size.height - user_speed)) {
+            user.pos.y += user_speed;
         }
     }
     gtk_widget_queue_draw(widget);
     return FALSE;
 }
 
-static void init_window() {
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Pong");
-    gtk_window_set_default_size(GTK_WINDOW(window), FIELD_WIDTH, FIELD_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+static gboolean update_game(gpointer user_data) {
+    update_ball_pos();
+    check_wall_collision();
+    check_goal();
+    check_player_collision(&user);
+    check_player_collision(&enemy);
+    update_enemy();
+    
+    gtk_widget_queue_draw(drawing_area);
+    return TRUE;
 }
 
-static void init_drawing_area() {
-    drawing_area = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(window), drawing_area);
-    g_signal_connect(drawing_area, "draw", G_CALLBACK(on_draw_event), &user);
+static void update_ball_pos() {
+    ball.pos.x += ball.dx;
+    ball.pos.y += ball.dy;
 }
 
-static void init_key_presses() {
-    g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press_event), NULL);
-    gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
+static void check_wall_collision() {
+    if (ball.pos.y - ball.radius <= 0 || ball.pos.y + ball.radius >= FIELD_HEIGHT) {
+        ball.dy = -ball.dy;
+    }
 }
 
-static void first_render() {
-    init_window();
-    init_drawing_area();
-    init_key_presses();
-    gtk_widget_show_all(GTK_WIDGET(window));
-    g_timeout_add(16, update_game, NULL);
-    gtk_main();
+static void check_goal() {
+    if (ball.pos.x - (ball.radius / 2) <= 0 || ball.pos.x + (ball.radius / 2) >= FIELD_WIDTH) {
+        ball.pos.x = (FIELD_WIDTH / 2) - (ball_radius / 2);
+        ball.pos.y = (FIELD_HEIGHT / 2) - (ball_radius / 2);
+        ball.radius = ball_radius;
+        ball.dx = -10;
+        ball.dy = 0;
+        ball.speed = 10;  
+    }
 }
 
-static void init_player_struct(Player *p, int num, int x, int y, int width, int height) {  
-    p->number = num;
-    p->pos.x = x;
-    p->pos.y = y;
-    p->size.width = width;
-    p->size.height = height;
+static void check_player_collision(Player *player) {
+    if (ball.pos.x - ball.radius <= player->pos.x + player->size.width && ball.pos.x + ball.radius >= player->pos.x) {
+        if (ball.pos.y >= player->pos.y && ball.pos.y <= player->pos.y + player->size.height) {
+            float hit_position = (ball.pos.y - player->pos.y) / (float)player->size.height;
+            float bounce_angle = (hit_position - 0.5) * MAX_BOUNCE_ANGLE;
+
+            ball.dx = ball.speed * cos(bounce_angle) * (ball.dx < 0 ? 1 : -1);  
+            ball.dy = ball.speed * sin(bounce_angle);
+
+            if (player->number == 1) {
+                enemy_catch_up = !enemy_catch_up;
+            }
+        }
+    }
 }
 
-static void make_players() {
-    int x_user = 0 + GOAL_WIDTH;
-    int x_enemy = FIELD_WIDTH - WIDTH_ENEMY - GOAL_WIDTH;
-    int y_user = (FIELD_HEIGHT / 2) - (HEIGHT_USER / 2);
-    int y_enemy = (FIELD_HEIGHT / 2) - (HEIGHT_ENEMY / 2);
+static void update_enemy() {
+    int catch_offset = enemy_speed;
+    if (enemy_catch_up) catch_offset = -enemy_speed; 
+    int target_y = predict_ball_y() - (enemy.size.height / 2) - catch_offset;
 
-    init_player_struct(&user, 0, x_user, y_user, WIDTH_USER, HEIGHT_USER);
-    init_player_struct(&enemy, 1, x_enemy, y_enemy, WIDTH_ENEMY, HEIGHT_ENEMY);
+    if (enemy.pos.y < target_y && enemy.pos.y <= (FIELD_HEIGHT - enemy.size.height - enemy_speed) ) {
+        enemy.pos.y += enemy_speed;
+        if (enemy.pos.y > target_y) enemy.pos.y = target_y;
+    } else if (enemy.pos.y > target_y && enemy.pos.y + enemy_speed > 0) {
+        enemy.pos.y -= enemy_speed;
+        if (enemy.pos.y < target_y) enemy.pos.y = target_y;
+    }
 }
 
-static void make_ball() {
-    ball.pos.x = (FIELD_WIDTH / 2) - (BALL_RAD / 2);
-    ball.pos.y = (FIELD_HEIGHT / 2) - (BALL_RAD / 2);
-    ball.radius = BALL_RAD;
-    ball.dx = -10;
-    ball.dy = 0;
-    ball.speed = 10;
-}
+static int predict_ball_y() {
+    float time_to_enemy = (enemy.pos.x - ball.pos.x) / ball.dx;
+    float predicted_y = ball.pos.y + time_to_enemy * ball.dy;
 
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
-    make_players();
-    make_ball();
-    first_render();
-    return 0;
+    while (predicted_y < 0 || predicted_y > FIELD_HEIGHT) {
+        if (predicted_y < 0) predicted_y = -predicted_y;
+        if (predicted_y > FIELD_HEIGHT) predicted_y = 2 * FIELD_HEIGHT - predicted_y;
+    }
+
+    return (int)predicted_y;
 }
